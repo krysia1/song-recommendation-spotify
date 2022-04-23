3bd4c8723d3f646dbeedeae700e6acc12f2fe23e
Krystyna Łosieczka <krysia@agabox.net>
2022-04-23 16:40:24 +0200

    add script to get currents user playing track

--- spotify.py
+++ spotify.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import urllib
 import webbrowser
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -13,22 +12,19 @@ load_dotenv()
 
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-print(CLIENT_SECRET)
 
 scope = 'user-library-read'
 token = util.prompt_for_user_token(
-    scope, 
-    client_id=CLIENT_ID, 
-    client_secret=CLIENT_SECRET, 
+    scope,
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
     redirect_uri='http://localhost:8080/callback'
-  )
+)
 spotify = spotipy.Spotify(auth=token)
 playlist_dic = {}
-playlist_cover_art = {}
 
 for i in spotify.current_user_playlists()['items']:
     playlist_dic[i['name']] = i['uri'].split(':')[2]
-    playlist_cover_art[i['uri'].split(':')[2]] = i['images'][0]['url']
 
 print("PLAYLISTS: ", playlist_dic)
 
@@ -44,14 +40,10 @@ def generate_playlist_df(playlist_name, playlist_dic):
         playlist.loc[i, 'image_url'] = j['track']['album']['images'][1]['url']
         playlist.loc[i, 'track_uri'] = j['track']['uri']
 
-
-    # print(playlist.loc[0, 'url'])
-    webbrowser.open(playlist.loc[0, 'track_uri'])
-
     return playlist
 
-playlist_df = generate_playlist_df('Stretching', playlist_dic) 
+playlist_df = generate_playlist_df('Stretching', playlist_dic)
 
-# print("PLAYLIST STRETCHING: ", playlist_df)
+print("PLAYLIST STRETCHING: ", playlist_df)
 
-# webbrowser.open(playlist_df[0])
+webbrowser.open(playlist_df.loc[0, 'track_uri'])
---
+++ spotifyNow.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy.util as util
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+playlist_dic = {}
+
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+client_secret=CLIENT_SECRET,
+redirect_uri='http://localhost:8080/callback',
+scope='user-read-currently-playing'))
+
+
+current_track = spotify.current_user_playing_track()
+
+if current_track == None:
+    print("CURRENTLY PLAYING: None")
+else:
+    current_track_name = spotify.current_user_playing_track()['item']['name']
+    artists = [artist for artist in spotify.current_user_playing_track()['item']['artists']]
+    current_track_artists = ', '.join([artist['name'] for artist in artists])
+    current_track_album = spotify.current_user_playing_track()['item']['album']['name']
+    print("CURRENTLY PLAYING: ", current_track_name, "by", current_track_artists, "from", current_track_album)
